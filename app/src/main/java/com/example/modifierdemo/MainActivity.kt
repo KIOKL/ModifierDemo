28373b711302838ada34a671ad2c71945fa509cd
KIOKL <ivan.kurasov99@mail.ru>
2026-09-09 11:46:30 +0700

    1 Обзор модификаторов

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.modifierdemo.ui.theme.ModifierDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +37,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
+    )
+}
+@Composable
+fun DemoScreen() {
+    Text(
+        "My Vacation",
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold
     )
 }
 
